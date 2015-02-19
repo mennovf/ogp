8d223df016b686a99dcdedb33980e29ac180f17f
mennovf <mennovanfrachem@hotmail.com>
2015-02-19 21:17:45 +0100

    Change private fields to public and remove getters/setters. Since this is just a pod type, setters and getters feel like overkill.

--- src/Transform.java
+++ src/Transform.java
@@ -1,6 +1,3 @@
-import be.kuleuven.cs.som.annotate.*;
-
-
 /**
  * A POD class representing a transform in a 2D world.
  * The transform bundles a position (x and y coordinate)
@@ -10,42 +7,12 @@ public class Transform {
 	public enum Direction{
 		LEFT, RIGHT
 	}
-	private double x = 0, y = 0;
-	private Direction facing;
+	public double x = 0, y = 0;
+	public Direction facing;
 	
 	public Transform(double x, double y, Direction dir){
-		this.setX(x);
-		this.setY(y);
-		this.setFacing(dir);
-	}
-
-	@Basic
-	public double getX() {
-		return x;
-	}
-
-	@Basic
-	public void setX(double x) {
 		this.x = x;
-	}
-
-	@Basic
-	public double getY() {
-		return y;
-	}
-
-	@Basic
-	public void setY(double y) {
 		this.y = y;
-	}
-
-	@Basic
-	public Direction getFacing() {
-		return facing;
-	}
-
-	@Basic
-	public void setFacing(Direction facing) {
-		this.facing = facing;
+		this.facing = dir;
 	}
 }

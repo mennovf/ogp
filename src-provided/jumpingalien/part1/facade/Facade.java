c2aa70d7f7e16517cc263149c92a8e7491ad91c7
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2015-02-20 14:03:15 +0100

    Add Facade for real

---
+++ src-provided/jumpingalien/part1/facade/Facade.java
@@ -0,0 +1,99 @@
+package jumpingalien.part1.facade;
+
+import jumpingalien.model.Mazub;
+import jumpingalien.util.Sprite;
+
+public class Facade implements IFacade {
+
+	@Override
+	public Mazub createMazub(int pixelLeftX, int pixelBottomY, Sprite[] sprites) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getLocation(Mazub alien) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getVelocity(Mazub alien) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getAcceleration(Mazub alien) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getSize(Mazub alien) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Sprite getCurrentSprite(Mazub alien) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void startJump(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endJump(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startMoveLeft(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endMoveLeft(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startMoveRight(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endMoveRight(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startDuck(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endDuck(Mazub alien) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void advanceTime(Mazub alien, double dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}

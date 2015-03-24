d3fe5055025f08c7aaaa698653b734171b56777a
mennovf <mennovanfrachem@hotmail.com>
2015-03-24 14:07:01 +0100

    Add health related facade method for Mazub.

--- src-provided/jumpingalien/part2/facade/Facade.java
+++ src-provided/jumpingalien/part2/facade/Facade.java
@@ -96,8 +96,7 @@ public class Facade implements IFacadePart2 {
 
 	@Override
 	public int getNbHitPoints(Mazub alien) {
-		// TODO Auto-generated method stub
-		return 0;
+		return alien.getHealth();
 	}
 
 	@Override

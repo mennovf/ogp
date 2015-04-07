325909cca214f1a2c2849b7ed879b8fac67892b2
mennovf <mennovanfrachem@hotmail.com>
2015-04-07 19:26:34 +0200

    Add tests for School public methods.

---
+++ tests/SchoolTest.java
@@ -0,0 +1,96 @@
+import static org.junit.Assert.*;
+import jumpingalien.model.School;
+import jumpingalien.model.Slime;
+import jumpingalien.model.Utilities;
+import jumpingalien.model.Vector;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class SchoolTest {
+
+	School school;
+	Slime slime;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		school = new School();
+		slime = Utilities.slime(new Vector<>(0.0, 0.0), school);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void canHaveAsSlime_valid(){
+		assertTrue(School.canHaveAsSlime(slime));
+	}
+	
+	@Test
+	public void canHaveAsSlime_invalid(){
+		assertFalse(School.canHaveAsSlime(null));
+		slime.terminate();
+		assertFalse(School.canHaveAsSlime(slime));
+	}
+	
+	@Test
+	public void addSlime_ok(){
+		school.addSlime(slime);
+		assertTrue(school.containsSlime(slime));
+		assertEquals(school, slime.getSchool());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void addSlime_invalid(){
+		school.addSlime(null);
+	}
+	
+	@Test
+	public void containSlime_contains(){
+		school.addSlime(slime);
+		assertTrue(school.containsSlime(slime));
+	}
+	
+	@Test
+	public void containsSlime_doesntContain(){
+		assertFalse(school.containsSlime(slime));
+		assertFalse(school.containsSlime(null));
+	}
+	
+	@Test
+	public void switchSchools_ok(){
+		int amount1 = 5;
+		for (int i = 0; i < amount1; ++i){
+			school.addSlime(Utilities.slime(new Vector<>(0.0, 0.0), school));
+		}
+		School school2 = new School();
+		int amount2 = 7;
+		for (int i = 0; i < amount2; ++i){
+			school2.addSlime(Utilities.slime(new Vector<>(0.0, 0.0), school));
+		}
+		school.addSlime(slime);
+		School.switchSchools(school, school2, slime);
+		assertTrue(school2.containsSlime(slime));
+		assertFalse(school.containsSlime(slime));
+		assertEquals(slime.getSchool(), school2);
+		assertEquals(slime.getHealth(), 100);
+		for (Slime schoolSlime : school.getSlimes()){
+			assertEquals(schoolSlime.getHealth(), 100);
+		}
+		for (Slime schoolSlime : school2.getSlimes()){
+			assertEquals(schoolSlime.getHealth(), 100 - 1);
+		}
+	}
+}

069c24200f4698257e4520efbd44e24b3276c359
mennovf <mennovanfrachem@hotmail.com>
2015-05-06 21:25:17 +0200

    Documentation for BinaryOperator and Literal.

--- src/jumpingalien/model/program/expression/BinaryOperation.java
+++ src/jumpingalien/model/program/expression/BinaryOperation.java
@@ -4,21 +4,49 @@ import java.util.function.BiFunction;
 
 
 
+/**
+ * A class representing a binary operation.
+ * 
+ * @author Rugen Heidbuchel & Menno Vanfrachem
+ *
+ * @param <R> The return type of this operation.
+ * 
+ * @param <V1> The type of the first parameter of this operation.
+ * 
+ * @param <V2> The Type of the second paramter of this operation.
+ */
 public class BinaryOperation<R, V1, V2> implements Expression<R> {
 	
 	private Expression<V1> left;
 	private Expression<V2> right;
 	private BiFunction<V1, V2, R> operation;
 	
+	/**
+	 * Constructs a new operation.
+	 * 
+	 * @param left
+	 * 			The left parameter of op.
+	 * 
+	 * @param right
+	 * 			The right parameter of op.
+	 * 
+	 * @param op
+	 * 			The operation to apply to left and right.
+	 */
 	public BinaryOperation(Expression<V1> left, Expression<V2> right, BiFunction<V1, V2, R> op) {
 		this.left = left;
 		this.right = right;
 		this.operation = op;
 	}
 
+	/**
+	 * Evaluates this expression.
+	 * 
+	 * @return Returns the result of op applied to left and right.
+	 * 			| op.apply(left.evaluate(), right.evaluate())
+	 */
 	@Override
 	public R evaluate() {
 		return operation.apply(left.evaluate(), right.evaluate());
 	}
-
 }
---
+++ src/jumpingalien/model/program/expression/Value.java
@@ -0,0 +1,34 @@
+package jumpingalien.model.program.expression;
+
+/**
+ * A expression representing a value.
+ * 
+ * @author Rugen Heidbuchel & Menno Vanfrachem
+ *
+ * @param <T>
+ * 			The type of the value it holds.
+ */
+public class Value<T> implements Expression<T> {
+	
+	private T val;
+	
+	/**
+	 * Construct a new Value.
+	 * 
+	 * @param val
+	 * 			The value 'this' will contain.
+	 */
+	public Value(T val) {
+		this.val = val;
+	}
+
+	/**
+	 * Returns the stored value.
+	 * 
+	 * @return Returns the stored value.
+	 */
+	@Override
+	public T evaluate() {
+		return val;
+	}
+}

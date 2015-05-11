42b2259aee39a79207f0e7535a770b9c3d2fae2e
mennovf <mennovanfrachem@hotmail.com>
2015-05-11 16:21:09 +0200

    Add Variable Expression (last expression?).

---
+++ src/jumpingalien/model/program/expression/Variable.java
@@ -0,0 +1,26 @@
+package jumpingalien.model.program.expression;
+
+import java.util.Map;
+
+/**
+ * A class representing the value of a variable.
+ */
+public class Variable implements Expression<Object> {
+	
+	private final String identifier;
+	
+	/**
+	 * Constructs a new Variable.
+	 * 
+	 * @param id
+	 * 			The identifier of this variable in the source code.
+	 */
+	public Variable(String id) {
+		this.identifier = id;
+	}
+
+	@Override
+	public Object evaluate(Map<String, Object> globals) {
+		return globals.get(this.identifier);
+	}
+}

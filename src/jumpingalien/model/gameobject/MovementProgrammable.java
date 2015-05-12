3980de64a6d1f8e5913f498f9d778f346256416e
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2015-05-12 20:25:39 +0200

    Add MovementProgrammable interface. An interface was better, because otherwise the constructor had to be overwritten. It also feels more logical to use an interface here.

---
+++ src/jumpingalien/model/gameobject/MovementProgrammable.java
@@ -0,0 +1,20 @@
+package jumpingalien.model.gameobject;
+
+/**
+ * An interface for defining movement programmable behaviour.
+ * This means a program can control the movement of the object
+ * that implements this interface.
+ * 
+ * @author Rugen Heidbuchel, Menno Vanfrachem
+ */
+public interface MovementProgrammable {
+	
+	public void startRun(double direction);
+	public void stopRun(double direction);
+	
+	public void startJump();
+	public void stopJump();
+	
+	public void startDuck();
+	public void stopDuck();
+}

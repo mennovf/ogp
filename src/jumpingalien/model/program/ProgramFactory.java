72b2684f2b2095895feb6a9277e7335baf0c38eb
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2015-05-12 13:48:51 +0200

    Implement skip

--- src/jumpingalien/model/program/ProgramFactory.java
+++ src/jumpingalien/model/program/ProgramFactory.java
@@ -327,8 +327,7 @@ public class ProgramFactory implements IProgramFactory<Expression<?>, Statement,
 
 	@Override
 	public Statement createSkip(SourceLocation sourceLocation) {
-		// TODO Auto-generated method stub
-		return null;
+		return new Wait(new Value<>(Statement.defaultTime));
 	}
 
 	@Override

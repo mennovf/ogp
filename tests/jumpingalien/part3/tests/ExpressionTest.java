5544d698cb9fe6b9671fa3d4f0e0800c520af68c
mennovf <mennovanfrachem@hotmail.com>
2015-05-13 18:43:12 +0200

    More Testst.

--- tests/jumpingalien/part3/tests/ExpressionTest.java
+++ tests/jumpingalien/part3/tests/ExpressionTest.java
@@ -3,6 +3,7 @@ package jumpingalien.part3.tests;
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
+import java.util.Random;
 
 import jumpingalien.model.program.Program;
 import jumpingalien.model.program.ProgramFactory;
@@ -44,12 +45,89 @@ public class ExpressionTest {
 		return new Program(mainStatement, globals);
 	}
 	
+	private <R> R runExpression(Expression<R> expr) {
+		Program p = new Program(new Break(), globals);
+		return expr.evaluate(this.globals, new CallStack(p));
+	}
 	
 	@Test
 	public void BinaryAddition_ok() {
 		Expression<Double> addition = pf.createAddition(new Value<Double>(3.0), new Value<Double>(1.0), sl);
-		HashMap<String, Object> globals = new HashMap<String, Object>();
-		Program p = new Program(new Break(), globals);
-		assertEquals(4.0, addition.evaluate(globals, new CallStack(p)), 1e-7);
+		assertEquals(4.0, this.runExpression(addition), 1e-7);
+	}
+	
+	
+	@Test
+	public void Variable_ok() {
+		this.globals.put("key", "variable");
+		assertEquals("variable", this.runExpression(new Variable<String>("key")));
+	}
+	
+	
+	@Test
+	public void Value_ok() {
+		assertEquals("string", this.runExpression(new Value<String>("string")));
+	}
+	
+	
+	@Test
+	public void Random_isRandom() {
+		assertNotEquals(this.runExpression(pf.createRandom(new Value<Double>(1.0), sl)),this.runExpression(pf.createRandom(new Value<Double>(1.0), sl)));
+	}
+	
+	
+	@Test
+	public void Random_maxValue() {
+		int amount = 30;
+		Double max = 10.0;
+		for (int i = 0 ; i<amount; ++i) {
+			assertTrue(this.runExpression(pf.createRandom(new Value<Double>(max), sl)) <= max);
+		}
+	}
+	
+	
+	@Test
+	public void Subtraction_ok() {
+		assertEquals(1.0, this.runExpression(pf.createSubtraction(new Value<Double>(2.0), new Value<Double>(1.0), sl)), 1e-7);
+	}
+	
+	@Test
+	public void Multiplication_ok() {
+		assertEquals(4.0, this.runExpression(pf.createMultiplication(new Value<Double>(3.0), new Value<Double>(4.0/3.0), sl)), 1e-7);
+	}
+	
+	
+	@Test
+	public void Division_ok() {
+		assertEquals(9.0/4.0, this.runExpression(pf.createDivision(new Value<Double>(3.0), new Value<Double>(4.0/3.0), sl)), 1e-7);
+	}
+	
+	
+	@Test
+	public void SquareRoot_ok() {
+		assertEquals(3.0, this.runExpression(pf.createSqrt(new Value<Double>(9.0), sl)), 1e-7);
+	}
+	
+	
+	@Test
+	public void And_ok() {
+		assertTrue(this.runExpression(pf.createAnd(new Value<Boolean>(true), new Value<Boolean>(true), sl)));
+		assertFalse(this.runExpression(pf.createAnd(new Value<Boolean>(false), new Value<Boolean>(true), sl)));
+	}
+	
+	
+	@Test
+	public void Or_ok() {
+		assertTrue(this.runExpression(pf.createOr(new Value<Boolean>(true), new Value<Boolean>(true), sl)));
+		assertTrue(this.runExpression(pf.createOr(new Value<Boolean>(false), new Value<Boolean>(true), sl)));
+		assertFalse(this.runExpression(pf.createOr(new Value<Boolean>(false), new Value<Boolean>(false), sl)));
+	}
+	
+	
+	
+	@Test
+	public void Negation_ok() {
+		assertTrue(this.runExpression(pf.createNot(new Value<Boolean>(false), sl)));
+		assertFalse(this.runExpression(pf.createNot(new Value<Boolean>(true), sl)));
 	}
 }
